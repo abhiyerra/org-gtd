c7619535dbedd183b6eac2025ee03181f7e2a4ad
Abhi Yerra <abhi.yerra@lookout.com>
2015-04-18 09:24:59 -0700

    initial

---
+++ org-gtd-pkg.el
@@ -0,0 +1,2 @@
+(define-package "org-gtd" "1.0"
+                "Utilities to help with GTD in org-mode")
---
+++ org-gtd.el
@@ -0,0 +1,84 @@
+;;; org-gtd.el --- Org-mode GTD review
+
+;; Copyright (C) 2015 Abhi Yerra
+
+;; Author: Abhi Yerra <abhi@berkeley.edu>
+;; Version: 1.0
+;; Keywords: gtd, orgmode
+;; URL: http://github.com/abhiyerra/org-gtd
+
+;;; Commentary:
+
+;; These are utilities to help do GTD under org-mode. One of the major
+;; tasks that is done in GTD is the daily review and the weekly
+;; review.
+
+
+(defun org-gtd-daily-review ()
+  (interactive)
+;; Anything that was due that wasn't done make sue that it can get
+;; done and updated. Make sure that it has an appropriate outcome and
+;; next-action.
+  )
+
+(defun org-gtd-task/single-stage-project ()
+  (org-entry-put (point) "NEXT_ACTION"
+                 (read-from-minibuffer
+                  (concat (nth 4 (org-heading-components))
+                          ". Next Action: ")))
+  (if (yes-or-no-p "Defer it?")
+      (progn
+        (org-schedule '(16 16))
+        (org-todo "DEFERRED"))
+    (progn
+      (org-deadline '(16 16))
+      (org-todo "NEXT-ACTION"))))
+
+(defun org-gtd-task/multi-stage-project ()
+  ;; Plan out the occurance to check if it has an outcome and
+  ;; next_action.
+  (org-todo "PLAN")
+  (org-deadline '(16 16)))
+
+(defun org-gtd-task/long-task ()
+  (org-entry-put (point)
+                 "OUTCOME"
+                 (read-from-minibuffer
+                  (concat (nth 4 (org-heading-components)) ". Outcome: ")))
+  (if (yes-or-no-p "Is it a multi-stage project?")
+      (org-gtd-task/multi-stage-project)
+    (org-gtd-task/single-stage-project)))
+
+
+(defun org-gtd-task-review ()
+  (interactive)
+  (if (yes-or-no-p (concat "Is "
+                           (nth 4 (org-heading-components))
+                           " an actionable task? "))
+      (if (yes-or-no-p "Will it take less than 2 mins?")
+          (message "Do it")
+        (org-gtd-task/long-task))))
+
+
+;; - [ ] Make sure that there are tags on everything.
+(defun org-gtd-review (todo-state)
+  (interactive)
+  (org-map-entries
+   '(lambda ()
+      (let ((cur-todo-state (nth 2 (org-heading-components)))
+            (cur-title (nth 4 (org-heading-components))))
+        (if cur-todo-state
+            (progn
+              (unless (org-entry-get (point) "OUTCOME")
+                (org-entry-put (point) "OUTCOME"
+                               (read-from-minibuffer (concat cur-title ". Outcome: "))))
+              (unless (org-entry-get (point) "NEXT_ACTION")
+                (org-entry-put (point) "NEXT_ACTION"
+                               (read-from-minibuffer (concat cur-title ". Next Action: "))))
+              ))))
+      todo-state 'agenda))
+
+  ;; If something is past the deadline review the nextaction and outcome. Update it.
+  ;;                            (if (or (eq (org-entry-get (point) "LastReviewed") nil)
+  ;; > (org-entry-get (point) "LastReviewed")  15 days
+  ;;                          )
